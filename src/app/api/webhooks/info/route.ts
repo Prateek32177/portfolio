eb77604aa9a03bcefb35143a1b1f8bc90b981a96
Prateek jain <prateek32177@gmail.com>
2026-02-19 22:22:13 +0530

    testing

--- src/app/api/webhooks/info/route.ts
+++ src/app/api/webhooks/info/route.ts
@@ -6,8 +6,6 @@ export const GET = async () => {
   const webhookSecretValue = await webhooksecret();
   
   console.log("Platform:", platformValue);
-  console.log("Webhook Secret:", webhookSecretValue);
-  
   return NextResponse.json({ 
     platform: platformValue, 
     webhooksecret: webhookSecretValue 
