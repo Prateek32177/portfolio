676a1ac56c3955b7b1937c6f21c694acd089a847
Prateek jain <prateek32177@gmail.com>
2026-02-26 01:04:01 +0530

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@upstash/qstash"],
 };
 
 export default nextConfig;
